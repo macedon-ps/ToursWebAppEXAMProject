91baeca13d4c74939d17ee2f44b557851e826645
Andrey Kazanchan <macedon1971@gmail.com>
2022-08-12 20:16:13 +0300

    some edits

--- ToursWebAppEXAMProject/Program.cs
+++ ToursWebAppEXAMProject/Program.cs
@@ -41,7 +41,7 @@ app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(
-	name: "research",
+	name: "search",
 	pattern: "{controller=Search}/{action=Index}/{id?}");
 app.MapControllerRoute(
 	name: "admin",
