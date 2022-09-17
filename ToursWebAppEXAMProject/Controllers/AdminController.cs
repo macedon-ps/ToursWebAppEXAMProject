472aeb6d4bce8a43b7cf8479f1837a6a529fc79f
Andrey Kazanchan <macedon1971@gmail.com>
2022-09-17 18:48:52 +0300

    changed AdminController and Index view, created views EditMenu and GetQueryResultNews

--- ToursWebAppEXAMProject/Controllers/AdminController.cs
+++ ToursWebAppEXAMProject/Controllers/AdminController.cs
@@ -16,15 +16,47 @@ namespace ToursWebAppEXAMProject.Controllers
 			this.DataManager = DataManager;
 		}
 
+		[HttpGet]
 		public IActionResult Index()
 		{
 			logger.Trace("Переход по маршруту /Admin/Index. Возвращено представление Admin/Index.cshtml\n");
 			Console.WriteLine("Переход по маршруту /Admin/Index. Возвращено представление Admin/Index.cshtml\n");
 			
-			var products = DataManager.productBaseInterface.GetAllItems();
-			
-			return View(products);
+			return View();
 		}
 
+		[HttpGet]
+		public IActionResult EditMenu()
+		{
+			logger.Trace("Переход по маршруту /Admin/EditMenu. Возвращено представление Admin/EditMenu.cshtml\n");
+			Console.WriteLine("Переход по маршруту /Admin/EditMenu. Возвращено представление Admin/EditMenu.cshtml\n");
+			
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult GetQueryResultNews(bool isFullName, string fullNameOrKeywordOfItem)
+		{
+			logger.Trace("Переход по маршруту /Admin/GetQueryResultNews");
+			Console.WriteLine("Переход по маршруту /Admin/GetQueryResultNews");
+
+			var news = DataManager.NewBaseInterface.GetQueryResultItems(fullNameOrKeywordOfItem, isFullName);
+
+			if (news == null)
+			{
+				logger.Warn("Возвращено представление /Error.cshtml\n");
+				Console.WriteLine("Возвращено представление /Error.cshtml\n");
+
+				// задаем входные параметры для объекта ErrorViewModel
+				// можно передать: modelType, id - обязательные параметры, message - опционально;
+				// message = "";
+				var errorInfo = new ErrorViewModel(typeof(List<New>));
+				return View("Error", errorInfo);
+			}
+
+			logger.Debug("Возвращено представление /Admin/GetQueryResultNews.cshtml\n");
+			Console.WriteLine("Возвращено представление /Admin/GetQueryResultNews.cshtml\n");
+			return View(news);
+		}
 	}
 }
