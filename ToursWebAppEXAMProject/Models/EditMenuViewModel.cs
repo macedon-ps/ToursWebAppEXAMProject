55840fbf985ca105ee52a696495977c0eb27019a
Andrey Kazanchan <macedon1971@gmail.com>
2022-09-17 18:36:49 +0300

    created EditMenuViewModel

---
+++ ToursWebAppEXAMProject/Models/EditMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ToursWebAppEXAMProject.Models
+{
+	public class EditMenuViewModel
+	{
+		public bool isFullName { get; set; }
+
+		public string fullNameOrKeywordOfItem { get; set; }	
+
+		public EditMenuViewModel(string fullNameOrKeywordOfItem, bool isFullName)
+		{
+			this.fullNameOrKeywordOfItem = fullNameOrKeywordOfItem;
+			this.isFullName = isFullName;
+		}
+
+	}
+}
