0da32a5bb66acb2d888da8ab676a851e4db24d89
Andrey Kazanchan <macedon1971@gmail.com>
2022-10-11 00:19:12 +0300

    added validation for form models

--- ToursWebAppEXAMProject/Models/New.cs
+++ ToursWebAppEXAMProject/Models/New.cs
@@ -16,13 +16,16 @@ namespace ToursWebAppEXAMProject.Models
         [StringLength(200, ErrorMessage = "Заголовок новости не должен содержать более 200 символов")]
         public string Name { get; set; } = "Заголовок новости";
 
+        [Required(ErrorMessage = "Введите краткое описание новости")]
         [Display(Name = "Краткое описание новости")]
         [StringLength(400, ErrorMessage = "Краткое описание новости не должно содержать более 400 символов")]
         public string ShortDescription { get; set; } = "Краткое описание новости";
 
+        [Required(ErrorMessage = "Введите полное описание новости")]
         [Display(Name = "Полное описание новости")]
         public string FullDescription { get; set; } = "Полное описание новости";
 
+        [Required(ErrorMessage = "Выберите титульную картинку новости")]
         [Display(Name = "Титульная картинка")]
         [StringLength(100, ErrorMessage = "Путь к титульной картинке не должен содержать более 100 символов")]
         public string? TitleImagePath { get; set; } = "Нет титульной картинки";
--- ToursWebAppEXAMProject/Models/Product.cs
+++ ToursWebAppEXAMProject/Models/Product.cs
@@ -20,13 +20,16 @@ namespace ToursWebAppEXAMProject.Models
         [StringLength(200, ErrorMessage = "Название туристического продукта не должно содержать более 200 символов")]
         public string Name { get; set; } = "Название туристического продукта";
 
-        [Display(Name = "Короткое описание туристического продукта")]
+        [Required(ErrorMessage = "Введите краткое описание туристического продукта")]
+        [Display(Name = "Краткое описание туристического продукта")]
         [StringLength(400, ErrorMessage = "Краткое описание туристического продукта не должно содержать более 400 символов")]
         public string ShortDescription { get; set; } = "Краткое описание туристического продукта";
 
+        [Required(ErrorMessage = "Введите полное описание туристического продукта")]
         [Display(Name = "Полное описание туристического продукта")]
         public string FullDescription { get; set; } = "Полное описание туристического продукта";
 
+        [Required(ErrorMessage = "Выберите титульную картинку туристического продукта")]
         [Display(Name = "Титульная картинка")]
         [StringLength(100, ErrorMessage = "Путь к титульной картинке не должен содержать более 100 символов")]
         public string? TitleImagePath { get; set; } = "Нет титульной картинки";
