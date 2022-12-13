15912b2dfe9d8ce7b0b39b98f7e105a8a3c91400
Andrey Kazanchan <macedon1971@gmail.com>
2022-12-13 15:20:07 +0200

    added SearchProductViewModel

---
+++ ToursWebAppEXAMProject/Models/SearchProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ToursWebAppEXAMProject.Models
+{
+	public class SearchProductViewModel
+	{
+		public string? CountryNameFromSelectList { get; set; } = "Страна";
+
+		public DateTime? DaysInterval { get; set; }
+
+		public NumberOfDays? NumberOfDaysFromSelectList { get; set; } = NumberOfDays.Один;
+
+		public string? NumberOfPeopleFromSelectList { get; set; } = "Количество взрослых и детей";
+
+		public List<string>? CitiesFromSelectList { get; set; } = new List<string>() { "Список городов"};
+
+	public string? MapImagePath { get; set; } = "Расположение карты";
+
+		public string? CountryDescription { get; set; } = "Краткое описание страны";
+
+		public string? CityDescrition { get; set; } = "Краткое описание города";
+
+		public string? LocalDescrition { get; set; } = "Краткое описание местной достопримечательности";
+
+		public List<string>? PicturesImagePath { get; set; } = new List<string>() { "Расположение картинок"};
+
+	}
+}
