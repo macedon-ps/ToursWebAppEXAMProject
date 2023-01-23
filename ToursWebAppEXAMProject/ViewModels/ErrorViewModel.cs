c5f073d9bbe3a9ff087cfea1017fbfba3550925b
Andrey Kazanchan <macedon1971@gmail.com>
2023-01-23 14:11:04 +0200

    created (changed) ErrorVIewModel and ModelsErrorViewModel, and its views

--- ToursWebAppEXAMProject/ViewModels/ErrorViewModel.cs
+++ ToursWebAppEXAMProject/ViewModels/ErrorViewModel.cs
@@ -1,61 +1,25 @@
-using System.ComponentModel.DataAnnotations;
+п»їusing System.ComponentModel.DataAnnotations;
 
 namespace ToursWebAppEXAMProject.ViewModels
 {
 	public class ErrorViewModel
 	{
 		/// <summary>
-		/// Передаваемый в GET запросе id какой-то сущности 
-		/// </summary> 
-		[Display(Name = "id сущности, вызвавшей ошибку")]
-		public int? RequestId { get; set; }
+		/// РЎРѕРѕР±С‰РµРЅРёРµ РѕР± РѕС€РёР±РєРµ
+		/// </summary>
+		[Display(Name = "РЎРѕРѕР±С‰РµРЅРёРµ РѕР± РѕС€РёР±РєРµ")]
+		public string? ErrorMessage { get; set; }
 
 		/// <summary>
-		/// Сообщение, передаваемое во вью ошибки
+		/// Р”Р°С‚Р° Рё РІСЂРµРјСЏ РІРѕР·РЅРёРєРЅРѕРІРЅРёСЏ РѕС€РёР±РєРё
 		/// </summary>
-
-		[Display(Name = "Сообщение об ошибке")]
-		public string? MessageToErrorViewModel { get; set; }
-
-		/// <summary>
-		/// Тип модели, кот. вызывает ошибку
-		/// </summary>
-		[Display(Name = "Тип данных (класс), вызвавший ошибку")]
-		public string ModelTypeCalledError { get; set; } = null!;
-
-		/// <summary>
-		/// Дата и время возникновения ошибки
-		/// </summary>
-		[Display(Name = "Дата и время ошибки")]
+		[Display(Name = "Р”Р°С‚Р° Рё РІСЂРµРјСЏ РѕС€РёР±РєРё")]
 		public DateTime DateTimeError { get; set; }
 
-		/// <summary>
-		/// Метод IsRequestId(int? requestId), кот. возвращает булевое значение, было ли id какой-то сущности
-		/// </summary>
-		/// <param name="requestId">id какой-то сущности</param>
-		/// <returns></returns>
-		public bool IsRequestId(int? requestId)
-		{
-			if (requestId == null) return false;
-			return true;
-		}
-
-		/// <summary>
-		/// Метод IsErrorMessage, кот. возвращает булевое значение, было ли сообщение об ошибке
-		/// </summary>
-		public bool IsErrorMessage => !string.IsNullOrEmpty(MessageToErrorViewModel);
-
-		public ErrorViewModel(Type modelType, int id, string? message = "")
+		public ErrorViewModel(string? errorMessage)
 		{
 			DateTimeError = DateTime.Now;
-			ModelTypeCalledError = modelType.ToString();
-			RequestId = id;
-			if (message != null && id != 0) MessageToErrorViewModel = message ?? $"вывод значения сущности с id = {id} из БД невозможен";
-		}
-
-		public ErrorViewModel(Type modelType, string? message = "") : this(modelType, 0, message)
-		{
-			if (message != null) MessageToErrorViewModel = message ?? "вывод значений сущностей из БД невозможен";
+			ErrorMessage = errorMessage;
 		}
 	}
 }
