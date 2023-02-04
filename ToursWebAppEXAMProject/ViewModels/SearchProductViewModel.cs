c2dc12867ff1d8c08bad8953d43aa06a792e94e1
Andrey Kazanchan <macedon1971@gmail.com>
2023-02-05 01:55:51 +0200

    changed SearchProductViewModel

--- ToursWebAppEXAMProject/ViewModels/SearchProductViewModel.cs
+++ ToursWebAppEXAMProject/ViewModels/SearchProductViewModel.cs
@@ -1,38 +1,109 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ToursWebAppEXAMProject.Controllers;
 using ToursWebAppEXAMProject.EnumsDictionaries;
+using ToursWebAppEXAMProject.Repositories;
 
 namespace ToursWebAppEXAMProject.ViewModels
 {
 	public class SearchProductViewModel
 	{
+		/// <summary>
+		/// Id страны
+		/// </summary>
+		public int CountryId { get; set; }
+
+		/// <summary>
+		/// Название выбранной страны
+		/// </summary>
 		[Display(Name = "Страна:")]
-		public CountriesListEnum? CountryNameFromSelectList { get; set; } = CountriesListEnum.Украина;
-		
+		public string? CountryNameSelected { get; set; } 
+
+		/// <summary>
+		/// Список стран в формате List<string>
+		/// </summary>
+		public List<string>? Countries { get; set; } = null!;
+
+		public string? CountriesOneString { get; set; }
+
+		/// <summary>
+		/// Список стран в формате SelectList
+		/// </summary>
+		public SelectList? CountriesList { get; set; } = null!;
+
+		public string? CountriesListOneString { get; set; }
+
+		/// <summary>
+		/// Событие изменения списка стран
+		/// </summary>
+		//public event Action? SelectCountryChangeEvent;
+
+		/// <summary>
+		/// Название выбранного города
+		/// </summary>
+		[Display(Name = "Город:")]
+		public string? CityNameSelected { get; set; } 
+
+		/// <summary>
+		/// Список городов выбранной страны в формате List<string>
+		/// </summary>
+		public List<string>? Cities { get; set; } = null!;
+
+		public string? CitiesOneString { get; set; }
+
+		/// <summary>
+		/// Список городов выбранной страны в формате SelectList
+		/// </summary>
+		public SelectList? CitiesList { get; set; } = null!;
+
+		public string? CitiesListOneString { get; set; }
+
+		/// <summary>
+		/// Выбранные даты отдыха
+		/// </summary>
 		[Display(Name = "Даты поездки:")]
-		public DateTime? DaysInterval { get; set; } = DateTime.Now;
+		// TODO: нужно доработать данный параметр DaysInterval / возможно это только дата начала ???
+		public DateTime DaysInterval { get; set; } = DateTime.Now;
 
+		/// <summary>
+		/// Количество дней отдыха
+		/// </summary>
 		[Display(Name = "Дней поездки:")]
-		public NumberOfDaysEnum? NumberOfDaysFromSelectList { get; set; } = NumberOfDaysEnum.Шесть_Восемь;
+		public NumberOfDaysEnum NumberOfDaysFromSelectList { get; set; } = NumberOfDaysEnum.Шесть_Восемь;
 
+		/// <summary>
+		/// Количество взрослых и детей
+		/// </summary>
 		[Display(Name = "Взрослых и детей:")]
-		public NumberOfPeopleEnum? NumberOfPeopleFromSelectList { get; set; } = NumberOfPeopleEnum.Два_взрослых;
-
-		[Display(Name = "Города:")]
-		public CitiesListEnum? CityFromSelectList { get; set; } = CitiesListEnum.Киев;
-
-		[Display(Name = "Карта / фотографии:")]
-		public string? MapImagePath { get; set; } = "/images/Maps/UkraineMap.jpg";
+		public NumberOfPeopleEnum NumberOfPeopleFromSelectList { get; set; } = NumberOfPeopleEnum.Два_взрослых;
 		
+		/// <summary>
+		/// Адрес карты / или фотографии
+		/// </summary>
+		[Display(Name = "Карта / фотографии:")]
+		public string MapImagePath { get; set; } = "/images/Maps/UkraineMap.jpg";
+		
+		/// <summary>
+		/// Краткая информация о стране
+		/// </summary>
 		[Display(Name = "Информация о стране:")]
-		public string? CountryDescription { get; set; } = "Информация о стране:";
+		public string CountryDescription { get; set; } = "Информация о стране:";
 
+		/// <summary>
+		/// Краткая информация о городе
+		/// </summary>
 		[Display(Name = "Информация о городе:")]
-		public string? CityDescrition { get; set; } = "Информация о городе:";
+		public string CityDescrition { get; set; } = "Информация о городе:";
 
+		/// <summary>
+		/// Краткая информация о достопримечательности
+		/// </summary>
 		[Display(Name = "Достопримечательность:")]
-		public string? LocalDescrition { get; set; } = "Достопримечательность:";
+		public string LocalDescrition { get; set; } = "Достопримечательность:";
 
+		/// <summary>
+		/// Список адресов картинок / изображений
+		/// </summary>
 		public List<string>? PicturesImagePath { get; set; }
 
 	}
