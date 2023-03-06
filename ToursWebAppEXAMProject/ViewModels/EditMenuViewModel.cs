a0b542566abf12433868d5e4eb9919479b4d9fe1
Andrey Kazanchan <macedon1971@gmail.com>
2023-03-06 17:40:16 +0200

    some edits

--- ToursWebAppEXAMProject/ViewModels/EditMenuViewModel.cs
+++ ToursWebAppEXAMProject/ViewModels/EditMenuViewModel.cs
@@ -1,13 +1,31 @@
 ﻿namespace ToursWebAppEXAMProject.ViewModels
 {
+	/// <summary>
+	/// Класс EditMenuViewModel для отображения меню создания/редактирования/удаления новости/блога/турпродукта
+	/// </summary>
 	public class EditMenuViewModel
 	{
+		/// <summary>
+		/// Свойство isFullName, содержит true, если нужно ввести полное название, false - если нужно ввести ключевое слово (символ)
+		/// </summary>
 		public bool isFullName { get; set; }
 
+		/// <summary>
+		/// свойство fullNameOrKeywordOfItem, содержит полное название или ключевое слово
+		/// </summary>
 		public string fullNameOrKeywordOfItem { get; set; }
 
+		/// <summary>
+		/// тип данных
+		/// </summary>
 		public string type { get; set; }
 
+		/// <summary>
+		/// конструктор класса EditMenuViewModel(), кот. задает параметры для отображения меню создания/редактирования/удаления новости/блога/турпродукта
+		/// </summary>
+		/// <param name="isFullName">свойство, кот. указывает, что вводится - полное название (true) или ключевое слово (false)</param>
+		/// <param name="fullNameOrKeywordOfItem">ввод для поиска полного названия или ключевого слова</param>
+		/// <param name="type">тип данных</param>
 		public EditMenuViewModel(bool isFullName, string fullNameOrKeywordOfItem, string type)
 		{
 			this.isFullName = isFullName;
