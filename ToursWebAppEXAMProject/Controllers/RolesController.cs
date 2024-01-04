1dc55b439676f6e60a60f617debc6fb3c6ea4acc
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2024-01-04 15:14:35 +0200

    added UserController, RolesController, added viewmodels and views for them

---
+++ ToursWebAppEXAMProject/Controllers/RolesController.cs
@@ -0,0 +1,140 @@
+﻿using ToursWebAppEXAMProject.Models;
+using ToursWebAppEXAMProject.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAuthN_AuthZ.ConfirmEmail.Controllers
+{
+    //[Authorize(Roles = "superadmin")]
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<User> _userManager;
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Метод вывода списка всех ролей с возможностью их создания, редактирования, удаления
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index() => View(_roleManager.Roles.ToList());
+
+        /// <summary>
+        /// Метод создания новой роли, Get метод
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create() => View();
+
+        /// <summary>
+        /// Метод создания новой роли, Post метод
+        /// </summary>
+        /// <param name="name">название роли</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        /// <summary>
+        /// Метод удаления роли
+        /// </summary>
+        /// <param name="id">идентификатор роли</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Метод вывода списка всех пользователей с возможностью устанавливать им роли
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult UserList() => View(_userManager.Users.ToList());
+
+        /// <summary>
+        /// Метод установки ролей для пользователей, Get метод
+        /// </summary>
+        /// <param name="userId">идентификатор пользователя</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Edit(string userId)
+        {
+            // получаем пользователя
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // получем список ролей пользователя
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Метод установки ролей для пользователей, Post метод
+        /// </summary>
+        /// <param name="userId">идентификатор пользователя</param>
+        /// <param name="roles">список ролей</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            // получаем пользователя
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // получем список ролей пользователя
+                var userRoles = await _userManager.GetRolesAsync(user);
+                // получаем все роли
+                var allRoles = _roleManager.Roles.ToList();
+                // получаем список ролей, которые были добавлены
+                var addedRoles = roles.Except(userRoles);
+                // получаем роли, которые были удалены
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("UserList");
+            }
+
+            return NotFound();
+        }
+    }
+}
+
---
+++ ToursWebAppEXAMProject/Controllers/UsersController.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToursWebAppEXAMProject.Models;
+using ToursWebAppEXAMProject.ViewModels;
+
+namespace ToursWebAppEXAMProject.Controllers
+{
+    //[Authorize(Roles ="superadmin,admin")]
+    public class UsersController : Controller
+    {
+        UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Метод вывода списка всех пользователей с возможностью их создания, редактирования, удаления
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index() => View(_userManager.Users.ToList());
+
+        /// <summary>
+        /// Метод вывода только списка всех пользователей
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult UsersList() => View(_userManager.Users.ToList());
+        
+        /// <summary>
+        /// Метод создания нового пользователя, Get метод
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create() => View();
+
+        /// <summary>
+        /// Метод создания нового пользователя, Post метод
+        /// </summary>
+        /// <param name="model">вью модель для создания нового пользователя</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email, BirthYear = model.Year };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        /// <summary>
+        /// Метод редактирования данных пользователя, Get метод
+        /// </summary>
+        /// <param name="id">идентификато пользователя</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Edit(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, Year = user.BirthYear };
+            return View(model);
+        }
+
+        /// <summary>
+        /// Метод редактирования данных пользователя, Post метод
+        /// </summary>
+        /// <param name="model">вью модель для редактирования данных пользователя</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.BirthYear = model.Year;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        /// <summary>
+        /// Метод удаления пользователя
+        /// </summary>
+        /// <param name="id">идентификатор пользователя</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Метод изменения пароля пользователя, Get метод
+        /// </summary>
+        /// <param name="id">идентификатор пользователя</param>
+        /// <returns></returns>
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ChangePasswordViewModel model = new ChangePasswordViewModel { Id = user.Id, Email = user.Email };
+            return View(model);
+        }
+
+        /// <summary>
+        /// Метод изменения пароля пользователя, Post метод
+        /// </summary>
+        /// <param name="model">вью модель для изменения пароля пользователя</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var _passwordValidator =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordValidator<User>)) as IPasswordValidator<User>;
+                    var _passwordHasher =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordHasher<User>)) as IPasswordHasher<User>;
+
+                    IdentityResult result =
+                        await _passwordValidator.ValidateAsync(_userManager, user, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        user.PasswordHash = _passwordHasher.HashPassword(user, model.NewPassword);
+                        await _userManager.UpdateAsync(user);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+            }
+            return View(model);
+        }
+    }
+}
