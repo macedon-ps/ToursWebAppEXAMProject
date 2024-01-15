c2b533b64d52e12b45397b5a83cdec4d6ba74156
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2024-01-15 18:35:30 +0200

    added CountriesController, CitiesController and all views for them

---
+++ ToursWebAppEXAMProject/Controllers/CitiesController.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToursWebAppEXAMProject.EnumsDictionaries;
+using ToursWebAppEXAMProject.Interfaces;
+using ToursWebAppEXAMProject.Models;
+using ToursWebAppEXAMProject.ViewModels;
+using static ToursWebAppEXAMProject.LogsMode.LogsMode;
+
+namespace ToursWebAppEXAMProject.Controllers
+{
+    public class CitiesController : Controller
+    {
+        IBaseInterface<City> _AllCities;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public CitiesController(IBaseInterface<City> Cities, IWebHostEnvironment hostingEnvironment)
+        {
+            this._AllCities = Cities;
+            this._hostingEnvironment = hostingEnvironment;
+        }
+
+        /// <summary>
+        /// Метод вывода всех городов
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult GetAllCities()
+        {
+            WriteLogs("Переход по маршруту /Cities/GetAllCities. ", NLogsModeEnum.Trace);
+
+            var cities = _AllCities.GetAllItems();
+
+            if (cities == null)
+            {
+                var errorMessage = "В БД нет ни одного города";
+                var errorInfo = new ErrorViewModel(errorMessage);
+
+                WriteLogs($"{errorMessage}. Возвращено ../Shared/Error.cshtml\n", NLogsModeEnum.Warn);
+
+                return View("../Shared/Error", errorInfo);
+            }
+
+            WriteLogs("Выводятся все города\n", NLogsModeEnum.Debug);
+
+            return View(cities);
+        }
+
+        /// <summary>
+        /// Метод вывода города по его id
+        /// </summary>
+        /// <param name="id">уникальный идентификатор города</param>
+        /// <returns></returns>
+        public IActionResult GetCity(int id)
+        {
+            WriteLogs($"Переход по маршруту /Cities/GetCity?id={id}. ", NLogsModeEnum.Trace);
+
+            var city = _AllCities.GetItemById(id);
+
+            if (city.Id == 0)
+            {
+                var errorMessage = $"В БД нет города с id = {id}";
+                var errorInfo = new ErrorViewModel(errorMessage);
+
+                WriteLogs($"{errorMessage}. Возвращено ../Shared/Error.cshtml\n", NLogsModeEnum.Warn);
+
+                return View("../Shared/Error", errorInfo);
+            }
+
+            WriteLogs($"Выводится город с id = {id}.\n", NLogsModeEnum.Debug);
+
+            return View(city);
+        }
+
+        /// <summary>
+        /// Метод создания города
+        /// </summary>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        public IActionResult CreateCity()
+        {
+            WriteLogs("Выполняется действие /Cities/CreateCity. ", NLogsModeEnum.Trace);
+
+            var city = new City();
+
+            // TODO: вставить индекс страны при создании
+
+            WriteLogs("Возвращено /Cities/EditCity.cshtml\n", NLogsModeEnum.Trace);
+
+            return View("EditCity", city);
+        }
+
+        /// <summary>
+        /// Метод редактирования города по его id
+        /// </summary>
+        /// <param name="id">универсальный идентификатор города</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpGet]
+        public IActionResult EditCity(int id)
+        {
+            WriteLogs("Переход по маршруту /Cities/EditCity. ", NLogsModeEnum.Trace);
+
+            // TODO: вставить индекс страны при редактировании
+
+            var city = _AllCities.GetItemById(id);
+            
+            city.DateAdded = DateTime.Now;
+
+            return View(city);
+        }
+
+        /// <summary>
+        /// Метод вывода результатов выборки городов по тому, что ищем - полное название или ключевое слово (букву)
+        /// </summary>
+        /// <param name="isFullName">полное название - true, ключевое слово (буква) - false</param>
+        /// <param name="fullNameOrKeywordOfItem">текст для поиска</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpGet]
+        public IActionResult GetQueryResultCities(bool isFullName, string fullNameOrKeywordOfItem)
+        {
+            WriteLogs("Переход по маршруту /Cities/GetQueryResultCities. ", NLogsModeEnum.Trace);
+
+            var cities = _AllCities.GetQueryResultItemsAfterFullName(fullNameOrKeywordOfItem, isFullName);
+            var numberCities = cities.Count();
+
+            if (numberCities == 0)
+            {
+                var message = $"Нет городов по запросу \"{fullNameOrKeywordOfItem}\". Возвращено ../Shared/Nothing.cshtml\n";
+
+                WriteLogs(message, NLogsModeEnum.Warn);
+
+                var nothingInfo = new ErrorViewModel(message);
+                return View("../Shared/Nothing", nothingInfo);
+            }
+
+            WriteLogs($"Выводятся все города по запросу \"{fullNameOrKeywordOfItem}\".\n", NLogsModeEnum.Debug);
+
+            return View(cities);
+        }
+
+        /// <summary>
+        /// Метод удаления отдельного города по его id
+        /// </summary>
+        /// <param name="id">универсальный идентификатор города</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpGet]
+        public IActionResult DeleteCity(int id)
+        {
+            var city = _AllCities.GetItemById(id);
+            _AllCities.DeleteItem(city, id);
+
+            WriteLogs("Возвращено ../Shared/SuccessForDelete.cshtml\n", NLogsModeEnum.Trace);
+
+            return View("../Shared/SuccessForDelete", city);
+        }
+
+        /// <summary>
+        /// Метод сохранения города с данными, введенными пользователем
+        /// </summary>
+        /// <param name="city">Модель города</param>
+        /// <param name="formValues">Данные формы ввода типа IFormCollection</param>
+        /// <param name="changeTitleImagePath">Данные формы ввода типа IFormFile</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpPost]
+        public async Task<IActionResult> SaveCity(City city, IFormCollection formValues, IFormFile? changeTitleImagePath)
+        {
+            WriteLogs("Запущен процесс сохранения города в БД. ", NLogsModeEnum.Debug);
+
+            if (ModelState.IsValid)
+            {
+                WriteLogs("Модель City прошла валидацию. ", NLogsModeEnum.Debug);
+
+                // если мы хотим поменять картинку
+                if (changeTitleImagePath != null)
+                {
+                    var filePath = $"/images/CitiesTitleImages/{changeTitleImagePath.FileName}";
+
+                    using (var fstream = new FileStream(_hostingEnvironment.WebRootPath + filePath, FileMode.Create))
+                    {
+                        await changeTitleImagePath.CopyToAsync(fstream);
+
+                        WriteLogs($"Новая титульная картинка города сохранена по пути: {filePath}\n", NLogsModeEnum.Debug);
+                    }
+                    city.TitleImagePath = filePath;
+                }
+
+                city.FullDescription = formValues["fullInfoAboutCity"];
+                city.DateAdded = DateTime.Now;
+
+                _AllCities.SaveItem(city, city.Id);
+
+                WriteLogs("Город успешно сохранен в БД. ", NLogsModeEnum.Debug);
+                WriteLogs("Возвращено ../Shared/Success.cshtml\n", NLogsModeEnum.Trace);
+
+                return View("../Shared/Success", city);
+            }
+            else
+            {
+                WriteLogs("Модель City не прошла валидацию. ", NLogsModeEnum.Warn);
+                WriteLogs("Возвращено /Cities/EditCity.cshtml\n", NLogsModeEnum.Trace);
+
+                city.FullDescription = formValues["fullInfoAboutCity"];
+
+                return View("EditCity", city);
+            }
+        }
+    }
+}
---
+++ ToursWebAppEXAMProject/Controllers/CountriesController.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToursWebAppEXAMProject.EnumsDictionaries;
+using ToursWebAppEXAMProject.Interfaces;
+using ToursWebAppEXAMProject.Models;
+using ToursWebAppEXAMProject.ViewModels;
+using static ToursWebAppEXAMProject.LogsMode.LogsMode;
+
+namespace ToursWebAppEXAMProject.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly IBaseInterface<Country> _AllCountries;
+        private readonly IBaseInterface<City> _AllCities;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public CountriesController(IBaseInterface<Country> Countries, IBaseInterface<City> Cities, IWebHostEnvironment hostingEnvironment)
+        {
+            this._AllCountries = Countries;
+            this._AllCities = Cities;
+            this._hostingEnvironment = hostingEnvironment;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Метод вывода всех стран
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult GetAllCountries()
+        {
+            WriteLogs("Переход по маршруту /Countries/GetAllCountries. ", NLogsModeEnum.Trace);
+            
+            var countries = _AllCountries.GetAllItems();
+
+            if(countries == null)
+            {
+                var errorMessage = "В БД нет ни одной страны";
+                var errorInfo = new ErrorViewModel(errorMessage);
+
+                WriteLogs($"{errorMessage}. Возвращено ../Shared/Error.cshtml\n", NLogsModeEnum.Warn);
+
+                return View("../Shared/Error", errorInfo);
+            }
+
+            WriteLogs("Выводятся все страны\n", NLogsModeEnum.Debug);
+
+            return View(countries);
+        }
+
+        /// <summary>
+        /// Метод вывода страны по ее id
+        /// </summary>
+        /// <param name="id">уникальный идентификатор страны</param>
+        /// <returns></returns>
+        public IActionResult GetCountry(int id)
+        {
+            WriteLogs($"Переход по маршруту /Countries/GetCountry?id={id}. ", NLogsModeEnum.Trace);
+
+            var country = _AllCountries.GetItemById(id);
+            
+            var cities = _AllCities.GetAllItems().Where(c => c.CountryId == id);
+            country.Cities = cities;
+
+            if (country.Id == 0)
+            {
+                var errorMessage = $"В БД нет страны с id = {id}";
+                var errorInfo = new ErrorViewModel(errorMessage);
+
+                WriteLogs($"{errorMessage}. Возвращено ../Shared/Error.cshtml\n", NLogsModeEnum.Warn);
+
+                return View("../Shared/Error", errorInfo);
+            }
+
+            WriteLogs($"Выводится страна с id = {id}.\n", NLogsModeEnum.Debug);
+
+            return View(country);
+        }
+
+        /// <summary>
+        /// Метод создания страны
+        /// </summary>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        public IActionResult CreateCountry()
+        {
+            WriteLogs("Выполняется действие /Countries/CreateCountry. ", NLogsModeEnum.Trace);
+
+            var country = new Country();
+
+            WriteLogs("Возвращено /Countries/EditCountry.cshtml\n", NLogsModeEnum.Trace);
+
+            return View("EditCountry", country);
+        }
+
+        /// <summary>
+        /// Метод редактирования страны по ее id
+        /// </summary>
+        /// <param name="id">универсальный идентификатор страны</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpGet]
+        public IActionResult EditCountry(int id)
+        {
+            WriteLogs("Переход по маршруту /Countries/EditCountry. ", NLogsModeEnum.Trace);
+
+            var country = _AllCountries.GetItemById(id);
+
+            var cities = _AllCities.GetAllItems().Where(c => c.CountryId == id);
+            country.Cities = cities;
+            country.DateAdded = DateTime.Now;
+
+            return View(country);
+        }
+
+        /// <summary>
+        /// Метод вывода результатов выборки стран по тому, что ищем - полное название или ключевое слово (букву)
+        /// </summary>
+        /// <param name="isFullName">полное название - true, ключевое слово (буква) - false</param>
+        /// <param name="fullNameOrKeywordOfItem">текст для поиска</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpGet]
+        public IActionResult GetQueryResultCountries(bool isFullName, string fullNameOrKeywordOfItem)
+        {
+            WriteLogs("Переход по маршруту /Countries/GetQueryResultCountries. ", NLogsModeEnum.Trace);
+
+            var countries = _AllCountries.GetQueryResultItemsAfterFullName(fullNameOrKeywordOfItem, isFullName);
+            var numberCountries = countries.Count();
+
+            if (numberCountries == 0)
+            {
+                var message = $"Нет стран по запросу \"{fullNameOrKeywordOfItem}\". Возвращено ../Shared/Nothing.cshtml\n";
+
+                WriteLogs(message, NLogsModeEnum.Warn);
+
+                var nothingInfo = new ErrorViewModel(message);
+                return View("../Shared/Nothing", nothingInfo);
+            }
+
+            WriteLogs($"Выводятся все страны по запросу \"{fullNameOrKeywordOfItem}\".\n", NLogsModeEnum.Debug);
+
+            return View(countries);
+        }
+
+        /// <summary>
+        /// Метод удаления отдельной страны по ее id
+        /// </summary>
+        /// <param name="id">универсальный идентификатор страны</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpGet]
+        public IActionResult DeleteCountry(int id)
+        {
+            var country = _AllCountries.GetItemById(id);
+            _AllCountries.DeleteItem(country, id);
+
+            WriteLogs("Возвращено ../Shared/SuccessForDelete.cshtml\n", NLogsModeEnum.Trace);
+
+            return View("../Shared/SuccessForDelete", country);
+        }
+
+        /// <summary>
+        /// Метод сохранения страны с данными, введенными пользователем
+        /// </summary>
+        /// <param name="country">Модель страны</param>
+        /// <param name="formValues">Данные формы ввода типа IFormCollection</param>
+        /// <param name="changeTitleImagePath">Данные формы ввода типа IFormFile</param>
+        /// <returns></returns>
+        [Authorize(Roles = "superadmin,editor")]
+        [HttpPost]
+        public async Task<IActionResult> SaveCountry(Country country, IFormCollection formValues, IFormFile? changeTitleImagePath)
+        {
+            WriteLogs("Запущен процесс сохранения страны в БД. ", NLogsModeEnum.Debug);
+
+            if (ModelState.IsValid)
+            {
+                WriteLogs("Модель Country прошла валидацию. ", NLogsModeEnum.Debug);
+
+                // если мы хотим поменять картинку
+                if (changeTitleImagePath != null)
+                {
+                    var filePath = $"/images/CountriesTitleImages/{changeTitleImagePath.FileName}";
+
+                    using (var fstream = new FileStream(_hostingEnvironment.WebRootPath + filePath, FileMode.Create))
+                    {
+                        await changeTitleImagePath.CopyToAsync(fstream);
+
+                        WriteLogs($"Новая титульная картинка страны сохранена по пути: {filePath}\n", NLogsModeEnum.Debug);
+                    }
+                    country.TitleImagePath = filePath;
+                }
+
+                country.FullDescription = formValues["fullInfoAboutCountry"];
+                country.DateAdded = DateTime.Now;
+
+                _AllCountries.SaveItem(country, country.Id);
+
+                WriteLogs("Страна успешно сохранена в БД. ", NLogsModeEnum.Debug);
+                WriteLogs("Возвращено ../Shared/Success.cshtml\n", NLogsModeEnum.Trace);
+
+                return View("../Shared/Success", country);
+            }
+            else
+            {
+                WriteLogs("Модель Country не прошла валидацию. ", NLogsModeEnum.Warn);
+                WriteLogs("Возвращено /Countries/EditCountry.cshtml\n", NLogsModeEnum.Trace);
+
+                country.FullDescription = formValues["fullInfoAboutCountry"];
+
+                return View("EditCountry", country);
+            }
+        }
+
+
+    }
+}
