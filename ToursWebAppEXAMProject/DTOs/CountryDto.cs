a4fe5552819157f6cb12c7c435d4c5d0d1ed2a01
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2026-04-01 00:22:09 +0300

    added DtoModels

---
+++ ToursWebAppEXAMProject/DTOs/CountryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToursWebAppEXAMProject.DTOs
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Map { get; set; }
+
+        public List<CityDto> Cities { get; set; } = new();
+    }
+}
