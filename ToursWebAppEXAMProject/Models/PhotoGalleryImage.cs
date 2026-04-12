d1b9deaac75c70bfa3fcf9355c9255fef743f807
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2026-04-12 13:31:02 +0300

    added new models - AboutPageVersion, PhotoGalleryImage, changed EditAboutPageViewModel

---
+++ ToursWebAppEXAMProject/Models/PhotoGalleryImage.cs
@@ -0,0 +1,13 @@
+﻿namespace ToursWebAppEXAMProject.Models
+{
+    public class PhotoGalleryImage
+    {
+        public int Id { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public int AboutPageVersionId { get; set; }
+
+        public virtual AboutPageVersion AboutPageVersion { get; set; }
+    }
+}
