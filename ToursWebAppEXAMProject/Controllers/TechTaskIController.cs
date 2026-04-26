68c9e4816028ddaf3f188f6e891efe0b6d154295
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2026-04-26 15:49:59 +0300

    added TechTaskController and view TechTask

---
+++ ToursWebAppEXAMProject/Controllers/TechTaskIController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToursWebAppEXAMProject.Interfaces;
+using ToursWebAppEXAMProject.ViewModels;
+
+namespace ToursWebAppEXAMProject.Controllers
+{
+    public class TechTaskController : Controller
+    {
+        private readonly ITechTaskService _service;
+
+        public TechTaskController(ITechTaskService service)
+        {
+            _service = service;
+        }
+
+        // GET
+        public IActionResult Page(string pageName)
+        {
+            var model = _service.GetPage(pageName);
+
+            return View("TechTask", model);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Page(TechTaskPageViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Save(model);
+            }
+
+            return View("TechTask", model);
+        }
+    }
+}
