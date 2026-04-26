f2f09d17cf92cd812506e2e0b7b264e8d95bd09a
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2026-04-26 15:32:21 +0300

    added TechTaskPageViewModel

---
+++ ToursWebAppEXAMProject/ViewModels/TechTaskPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ToursWebAppEXAMProject.Models;
+
+namespace ToursWebAppEXAMProject.ViewModels
+{
+    public class TechTaskPageViewModel
+    {
+        public int Id { get; set; }
+
+        public string PageName { get; set; }
+
+        public List<TechTaskItem> Tasks { get; set; }
+
+        public double Progress { get; set; }
+    }
+}
