a79f915d08219a6d69958bc76b21034477a1d5ff
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2026-05-24 14:34:29 +0300

    added feature "where am i ?", changed view Index for SupportController, changed _Layout

---
+++ ToursWebAppEXAMProject/wwwroot/js/support-map.js
@@ -0,0 +1,52 @@
+﻿const mapPreview = document.getElementById("mapPreview");
+const buttonElement = document.getElementById("showLocationBtn");
+const mapContainer = document.getElementById("mapContainer");
+const mapElement = document.getElementById("supportMap");
+
+showLocationBtn.addEventListener("click", function () {
+
+    if (!mapElement)
+        return;
+
+    navigator.geolocation.getCurrentPosition(
+
+        function (position) {
+
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+
+            const map = L.map('supportMap')
+                .setView([lat, lng], 13);
+
+            L.tileLayer(
+                'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                {
+                    attribution: '© OpenStreetMap contributors'
+                }
+            ).addTo(map);
+
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup("Вы здесь")
+                .openPopup();
+
+            setTimeout(() => {
+                map.invalidateSize();
+            }, 100);
+
+        },
+
+        function (error) {
+
+            console.error(error);
+
+            alert("Не удалось получить геолокацию.");
+
+        }
+
+    );
+
+    mapPreview.style.display = "none";
+    mapContainer.style.display = "block";
+
+});
